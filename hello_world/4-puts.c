582110bb0f4fad4de99bc5d3cc16bf6860e00b04
Super User <5005@holbertonschool.com>
2022-06-07 11:52:54 +0000

    add step 5

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,17 @@
-#!/bin/bash
-#include<stdio.h>  
-int main () 
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle")
-	return 0;
+	char msg[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(msg);
+
+	return (0);
 }
