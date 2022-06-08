9fd471793f35f854515c360a0b9c9d9a9a4596ea
Super User <5005@holbertonschool.com>
2022-06-08 10:09:58 +0000

    add step 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	if (i == 0)
+	{
+		putchar('0');
+	}
+	if (I / 10 != 0)
+		putchar(i % 10 + '0');
+
+	return (0);
+}
