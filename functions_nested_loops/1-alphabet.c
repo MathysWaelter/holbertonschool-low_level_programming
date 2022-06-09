9633458da3d1ab1a6599095d23d1560533e5a98a
Super User <5005@holbertonschool.com>
2022-06-09 13:18:40 +0000

    add step 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	return (0);
+}
