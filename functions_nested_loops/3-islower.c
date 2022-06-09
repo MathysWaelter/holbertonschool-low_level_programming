15fa460aea7111d88783e7c85bc672f39d78ec09
Super User <5005@holbertonschool.com>
2022-06-09 15:56:36 +0000

    add step 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  *  - Print A-Z
+  *
+  * Return: Always 0 (Success)
+  */
+
+int _islower(int c)
+{
+	int a = 0;
+	char low;
+	
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low == c )
+			a = 1;
+	}
+
+	return (a);
+}	
