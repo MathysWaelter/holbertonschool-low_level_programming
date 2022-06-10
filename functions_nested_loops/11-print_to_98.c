e824f8d973e6f87b56fd15ccdc232a72cd417bd4
Super User <5005@holbertonschool.com>
2022-06-10 12:50:19 +0000

    add step 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,10 +11,17 @@
 void print_to_98(int n)
 {
 	while (n != 98)
+	{
 		if (n > 98)
+		{
+			printf("%d, ", n);
 			n--;
-		else if (n < 98)
+		}
+		else
+		{
+			printf("%d, ", n);
 			n++;
-	_putchar(n);
+		}
+	}
+	printf("98\n");
 }
-
