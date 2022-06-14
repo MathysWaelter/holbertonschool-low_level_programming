62abb3ae26ea1ff74be3acd7a8b0fd8d537ce051
Super User <5005@holbertonschool.com>
2022-06-14 08:15:05 +0000

    add step 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -10,9 +10,9 @@ void print_square(int size)
 	int a;
 	int b;
 
-	for(a = 1; a<=size; a++)
+	for (a = 1; a <= size; a++)
 	{
-		for (b = 1; b <=size; b++)
+		for (b = 1; b <= size; b++)
 		{
 			_putchar('#');
 		}
