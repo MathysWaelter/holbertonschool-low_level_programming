e9a42bbdecb6a0761ed4e72aaa9ad2f84899520b
Super User <5005@holbertonschool.com>
2022-06-14 12:24:52 +0000

    add step10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -17,12 +17,12 @@ void print_triangle(int size)
 	for (a = 1; a <= size; a++)
 	{
 		for (b = 1; b <= size; b++)
-		{	
+		{
 			if (b <= size - a)
 				_putchar(' ');
 			else
 				_putchar('#');
-		}	
+		}
 	_putchar('\n');
 	}
 }
