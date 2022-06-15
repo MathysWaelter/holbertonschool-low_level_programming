0e35f2b0047462f20398a170660848aef07262fc
Super User <5005@holbertonschool.com>
2022-06-15 16:09:37 +0000

    add step 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -4,11 +4,12 @@
  * *_strcpy - main
  * @dest: var
  * @src: var
+ * Return: Always 0.
  */
 
 char *_strcpy(char *dest, char *src)
 {
-        int temp;
+	int temp;
 
 	for (temp = 0; src[temp] != '\0'; temp++)
 	{
