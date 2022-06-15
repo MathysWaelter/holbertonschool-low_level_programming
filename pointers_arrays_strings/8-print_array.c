800c7898c8d8ca25873a47c33c12a44536f2290a
Super User <5005@holbertonschool.com>
2022-06-15 15:36:59 +0000

    add step 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -9,9 +9,17 @@
 
 void print_array(int *a, int n)
 {
-	for (n = 0; n < *a; n++)
+	int i;
+
+	for (i = 0; i < n; i++)
 	{
-		printf("%d ,", n[a]);
+		if (i == 0)
+		{
+			printf("%d", a[i]);
+		}
+		else
+			printf(", %d", a[i]);
 	}
+
 	printf("\n");
 }
