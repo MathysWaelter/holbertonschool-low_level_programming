424663194e0a7858e9bcfa63cf35cbb966676b22
Super User <5005@holbertonschool.com>
2022-06-15 14:12:11 +0000

    add step 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,10 +8,15 @@
 void puts_half(char *str)
 {
 	int i = 0;
+	int b;
 
+	for (b = 0; str[b] != '\0'; b++)
+	{
+	}
+	b = b - 1;
 	while (str[i] != '\0')
 	{
-		if (i / 5)
+		if (i > (b / 2))
 			_putchar(str[i]);
 		i++;
 	}
