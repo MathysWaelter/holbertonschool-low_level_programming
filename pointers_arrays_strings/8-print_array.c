ab71cb77efe2a41b94c18b1fa7f190941d8fa86b
Super User <5005@holbertonschool.com>
2022-06-15 15:25:14 +0000

    add step 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * print_array - print
+ * @a: var
+ * @n: var
+ */
+
+void print_array(int *a, int n)
+{
+	for (n = 0; n < *a; n++)
+	{
+		printf("%d ,", n[a]);
+	}
+	printf("\n");
+}
