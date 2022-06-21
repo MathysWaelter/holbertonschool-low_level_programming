6312ba94912182c10f4fed9317839360b76dfe3a
Super User <5005@holbertonschool.com>
2022-06-21 14:30:23 +0000

    add step 7

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -7,12 +7,19 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	unsigned int i, x;
+	int i, x;
 
 	for (i = 0; i < 8; i++)
 	{
 		for (x = 0; x < 8; x++)
+		{
+			if (x == 7)
+			{
 			_putchar(a[i][x]);
 			_putchar('\n');
+			}
+			else 
+				_putchar(a[i][x]);
 	}
+}
 }
