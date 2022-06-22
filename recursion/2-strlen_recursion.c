fb92f3042e21137702ebafd182037ddc0435cb08
Super User <5005@holbertonschool.com>
2022-06-22 12:33:34 +0000

    add step 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
-/*
- * _strlen_recursion - lenght of a string 
+/**
+ * _strlen_recursion - Lenght of a string
  * @s: var
- * Return . Always 0.
+ * Return: 0
  */
 
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return 0;
-	else 
-		return 1 + _strlen_recursion(s + 1);
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
