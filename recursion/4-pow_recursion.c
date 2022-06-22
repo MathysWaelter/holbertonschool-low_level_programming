bf3c27169b7513b3e3725f8b12deef4960296935
Super User <5005@holbertonschool.com>
2022-06-22 12:54:10 +0000

    add step 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - return the value 
+ * @x: var
+ * @y: var
+ * Return: 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int i;
+	int pow = 1;
+
+	for (i = 1; i <= y; i++)
+	{
+		pow = pow * x;
+	}
+	return (pow);
+}
+
