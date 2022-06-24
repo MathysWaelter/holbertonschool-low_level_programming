49560ed305d1a8685e77e0a74622828db9cab389
Super User <5005@holbertonschool.com>
2022-06-24 15:46:08 +0000

    add step 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - execute program
+ * @argc: int of argument
+ * @argv: char of argument
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int sum;
+	int i;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
