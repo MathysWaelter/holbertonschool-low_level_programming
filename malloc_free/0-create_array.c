648bc6aee3879aaa1cd225ca8d7e2abffb6b9ace
Super User <5005@holbertonschool.com>
2022-06-27 11:35:48 +0000

    add step 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *create_array - create an array of char
+ * @size: var
+ * @c: var
+ * Return: 0.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *tab;
+	unsigned int i;
+
+	tab = malloc(size * sizeof(char));
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		tab[i] = c;
+		i++;
+	}
+	return (tab);
+}
