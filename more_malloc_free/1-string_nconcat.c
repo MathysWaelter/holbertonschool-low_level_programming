7765e227640945daa2896bd34ab04fdabc56d7e7
Super User <5005@holbertonschool.com>
2022-06-29 14:32:52 +0000

    fix

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -19,37 +19,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s1 = "";
 	}
-
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-
 	for (c = 0; c < n ; c++)
 		;
-
 	for (i = 0; s1[i]; i++)
 		x++;
-
 	s3 = malloc(sizeof(char) * (x + n + 6));
 	if (s3 == NULL)
-	{
 		return (NULL);
-	}
-
 	x = 0;
-
 	for (i = 0; s1[i]; i++)
 	{
 		s3[x++] = s1[i];
 	}
-
 	for (i = 0; s2[i] && i < n; i++)
 	{
 		s3[x++] = s2[i];
 	}
-
 	s3[x] = '\0';
-
 	return (s3);
 }
