ddd8eabb4f41b57a34749a59e80e090a84fc9d5a
Super User <5005@holbertonschool.com>
2022-06-29 11:29:57 +0000

    add step 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,20 +1,17 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * *str_concat - concatenates
- * @s1: first string
- * @s2: second string
- * Return: 0.
+ * str_concat - concatenate two strings using malloc
+ * @s1: var
+ * @s2: var
+ * Return: pointer to concat string
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	int i, j;
-	int x;
-	int z;
+	char *a;
+	int i, j, c, d;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -25,19 +22,18 @@ char *str_concat(char *s1, char *s2)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
 		;
-	
-	s3 = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
-	if (s3 = NULL)
+
+	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
+	if (a == NULL)
 		return (NULL);
 
-	for (c = 0; x = 0; x < (i + j + 1); x++)
-		{
-			if (x < i)
-				s3[x] = s1[x];
-			else 
-				s3[x] = s2[z++];
-		}
-	return (s3);
+	for (c = 0, d = 0; c < (i + j + 1); c++)
+	{
+		if (c < i)
+			a[c] = s1[c];
+		else
+			a[c] = s2[d++];
+	}
+
+	return (a);
 }
-
-
