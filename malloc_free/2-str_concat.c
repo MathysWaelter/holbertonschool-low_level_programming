ef966d327a61c74399d05c121aee4cc63ee5a2d7
Super User <5005@holbertonschool.com>
2022-06-29 11:24:53 +0000

    step 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,24 +12,32 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s3;
-	int lenS1 = strlen(s1);
-	int lenS2 = strlen(s2);
-
+	int i, j;
+	int x;
+	int z;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	s3 = malloc(lenS1 + lenS2 + 1);
-	if (s3 == NULL)
-	{
-		free(s3);
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	
+	s3 = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
+	if (s3 = NULL)
 		return (NULL);
-	}
-	memcpy(s3, s1, lenS1);
-	memcpy(s3 + lenS1, s2, lenS2);
-	s3[lenS1 + lenS2] = '\0';
 
+	for (c = 0; x = 0; x < (i + j + 1); x++)
+		{
+			if (x < i)
+				s3[x] = s1[x];
+			else 
+				s3[x] = s2[z++];
+		}
 	return (s3);
 }
+
+
