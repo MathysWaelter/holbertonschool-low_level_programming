5dfbe6ad51d32bd53fbf80a11317ac0da294e0fa
Super User <5005@holbertonschool.com>
2022-06-30 09:47:24 +0000

    add step 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_calloc - allocates memory
+ * @nmemb: array
+ * @size: size of array
+ * Return: 0.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	int *p;
+
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * sizeof(unsigned int));
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		p[i] = nmemb * i;
+
+	(void)p;
+	return (p);
+}
