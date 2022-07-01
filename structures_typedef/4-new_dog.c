d3804ac70c4638d5e5f5717cb8d837874af86fa9
Mathys Waelter <5005@holbertonschool.com>
2022-07-01 14:24:55 +0000

    add step 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,41 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *new_dog - new dog
+ * @name: name of new dog
+ * @age age of new dog
+ * @owner: owner of new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i;
+	int x;
+	dog_t *new_dog;
+	new_dog = malloc(sizeof(dog_t));
+
+	i = strlen(name) + 1;
+	x = strlen(owner) + 1;
+
+	new_dog->name = malloc(sizeof(char) * i);
+	if (new_dog->name == NULL)
+		return (NULL);
+
+	strcpy(new_dog->name, name);
+	
+	new_dog->age = age;
+	
+	new_dog->owner = malloc(sizeof(char) * x);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		return (NULL);
+	}
+
+	strcpy(new_dog->owner, owner);
+
+	return (new_dog);
+}
