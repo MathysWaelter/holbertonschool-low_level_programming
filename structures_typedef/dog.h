ed8c959ed845d6fded47f4c87a2510a35657040e
Mathys Waelter <5005@holbertonschool.com>
2022-07-01 12:46:30 +0000

    add step 1

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,14 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 /**
  * struct dog - dog
  * @name: - dog name
  * @age: - dog age
  * @owner: - dog owner
  */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
 struct dog
 {
 	char *name;
