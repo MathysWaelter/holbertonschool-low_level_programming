71a8aa08ded3dc9a0bfc4265e595fa9df35851fb
Mathys Waelter <5005@holbertonschool.com>
2022-07-04 11:50:53 +0000

    add step 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * array_iterator - unction given as a parameter on each element of an array.
+ * @array: array
+ * @size: size of array
+ * @action: function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
