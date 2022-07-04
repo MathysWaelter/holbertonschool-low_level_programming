90697bd1a4349e93f9ce16b9e7679bc53d3d4076
Mathys Waelter <5005@holbertonschool.com>
2022-07-04 12:40:13 +0000

    add step 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * int_index - return the index of the first element 
@@ -13,15 +14,19 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 	int j = 0;
+	int y;
 
 	if (size <= 0 || cmp == NULL)
 		return (-1);
 
-	for (i = 0; i < size; i++)
-		if (cmp(array[i] == 98) || (cmp(array[i] == 1)))
+	for (i = 0,y = 0; i < size; i++)
+	{
+		y = cmp(array[i]);
+		if (y == 1)
 		{
 			j++;
 		}
+	}
 	if (j > 0)
 	{
 	return (j);
