895ccf4f2b3215ccd58de975709f86dde520bf5c
Mathys Waelter <5005@holbertonschool.com>
2022-07-05 17:25:43 +0000

    add step 3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum
+ * @a: num1
+ * @b: num2
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference
+ * @a: num1
+ * @b: num2
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product
+ * @a: num1
+ * @b: num2
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divisions
+ * @a: num1
+ * @b: num2
+ * Return: divisions
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo
+ * @a: num1
+ * @b: num2
+ * Return: modulo
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
