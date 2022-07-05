a1195e37d7c9d259da3e19065f1edcbe356fca9a
Mathys Waelter <5005@holbertonschool.com>
2022-07-05 14:34:52 +0000

    add step 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -3,17 +3,23 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * sum_them_all - sum
+ * @n: var
+ * Return: 0.
+ */
+
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int x = 0;
 	va_list sum;
 
-	if(n == 0)
+	if (n == 0)
 		return (0);
 
 	va_start(sum, n);
-	
+
 	for (i = 0; i < n; i++)
 	{
 		x += va_arg(sum, unsigned int);
