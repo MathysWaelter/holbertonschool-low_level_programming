b4f0818b7e1390719ce38f9090d57ec574391f95
Mathys Waelter <5005@holbertonschool.com>
2022-07-05 21:34:56 +0000

    add step 3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - prints anything
+ * @format: type
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *string;
+	char *empty = "";
+
+	va_list all;
+
+
+	va_start(all, format);
+
+	if (format)
+	{
+		while(format[i])
+		{
+			switch (format[i]) 
+			{
+				case 'c':
+					printf("%s%c", empty, va_arg(all, int));
+					break;
+				case 'i':
+					printf("%s%d", empty, va_arg(all, int));
+					break;
+				case 'f':
+					printf("%s%f", empty, va_arg(all, double));
+					break;
+				case 's':
+				string = va_arg(all, char *);
+				printf("%s%s", empty, string);
+					break;
+
+				default:
+				i++;
+				continue;
+				}
+			}
+			}
+		printf("\n");
+		va_end(all);
+}
