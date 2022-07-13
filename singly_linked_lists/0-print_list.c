907c8b42ff7252cef8fd286a87950d46d43295a3
Mathys Waelter <5005@holbertonschool.com>
2022-07-13 09:22:36 +0000

    add step 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -14,9 +14,9 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)\n");
-			h = h->next;
-			cpt++;
+		printf("[0] (nil)\n");
+		h = h->next;
+		cpt++;
 		}
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
