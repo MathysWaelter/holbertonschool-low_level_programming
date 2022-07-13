30d93922f3b9c8ed06ebc6bb54537de11f38f35a
Mathys Waelter <5005@holbertonschool.com>
2022-07-13 12:12:22 +0000

    add step 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+
+
+#include "lists.h"
+
+/**
+ * list_len - function that prints lenght of a singly linked list
+ * @h: node pointer
+ * Return: 0.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *node;
+
+	if (str == NULL)
+		return (NULL);
+
+	node = malloc(sizeof(list_t));
+	if (node == NULL)
+		return (NULL);
+
+
+	node->str = strdup(str);
+	if (node->str == NULL)
+		return (NULL);
+	
+	node->len = strlen(node->str);
+	node->next = *head;
+		*head = node;
+	return (node);
+}
