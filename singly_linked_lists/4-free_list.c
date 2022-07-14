a1b6ca389c0ea5e414fd976986f839d46d67aab7
Mathys Waelter <5005@holbertonschool.com>
2022-07-14 14:36:56 +0000

    fix step 4

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -11,9 +11,10 @@ void free_list(list_t *head)
 
 	while (head != NULL)
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
 	}
 }
 
