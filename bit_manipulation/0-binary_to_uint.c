0cd8dca6be76d5278fbab4f5ff21389d5857bb4d
Mathys Waelter <5005@holbertonschool.com>
2022-07-18 12:23:06 +0000

    add step 0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -7,23 +7,27 @@
 /**
  * binary_to_uint - convert binary to int
  * @b: is null
- * return: int
+ * Return: int
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int val;
-	int x;
+	unsigned int val = 0;
+	int x = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[x] == '0' || b[x] == '1')
+	for (x = 0; b[x] != '\0'; x++)
+	{
+		if (b[x] != '0' && b[x] != '1')
+			return (0);
+	}
+	for (x = 0; b[x] != '\0'; x++)
 	{
 		val = val << 1;
-		val = val + b[x]-'0';
-		x++;
+		if (b[x] == '1')
+			val = val + 1;
 	}
 	return (val);
 }
-
