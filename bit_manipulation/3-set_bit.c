17c79e87e0e00bc1be541453ec3687c9d7ab4ae6
Mathys Waelter <5005@holbertonschool.com>
2022-07-19 12:48:37 +0000

    add step 3

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+
+/**
+ * set_bit - returns the value of a bit
+ * @n: value
+ * @index: placement
+ * Return: Value of the bit
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >(sizeof(n) * 8))
+		return (-1);
+
+	*n |= 1 << index;
+	return (1);
+}
