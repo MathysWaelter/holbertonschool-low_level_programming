5de329d81b0043eae91e0b725c0eb2f0052bef1f
Mathys Waelter <5005@holbertonschool.com>
2022-07-20 20:28:20 +0000

    add step 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,35 +10,34 @@
 int create_file(const char *filename, char *text_content)
 {
 	int i, fd;
-	ssize_t wr;
+	int wr, j;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-	{
-		fd = open(filename, O_CREAT, 0600);
-		if (fd == -1)
-		{
-			return (-1);
-		}
-		return (1);
-	}
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	
 	if (fd == -1)
 		return (-1);
-
-	while (text_content[i] != 0)
+	
+	if (text_content == NULL)
 	{
-		i++;
+		j = open(filename, O_CREAT | O_RDONLY);
+		if (j == -1)
+			return (-1);
+		text_content = malloc(sizeof(char) * 1);
+		read(j, text_content, 0);
 	}
+	
+	for (i = 0; text_content[i]; i++)
+		;
 
 	wr = write(fd, text_content, i);
 
 	if (wr == -1)
 		return (-1);
-
+	
+	close(j);
 	close(fd);
 	return (1);
 }
