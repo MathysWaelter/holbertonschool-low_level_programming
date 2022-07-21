f591e53178e020c8a97e82bbc736be9e0f5e0bce
Mathys Waelter <5005@holbertonschool.com>
2022-07-21 11:25:32 +0000

    fix step 3

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -30,7 +30,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		if ((*head) == NULL)
 		{
 			(*head) = node;
-			return (0);
+			return (NULL);
 		}
 
 		while (tail->next != NULL)
