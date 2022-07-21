26135755a370a216dcb9acff27f70b277f57ca16
Mathys Waelter <5005@holbertonschool.com>
2022-07-21 08:51:45 +0000

    add step 1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - return number of element in list
+ * @h: imput
+ * Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int cpt = 0;
+
+	while (h)
+	{
+		h = h -> next;
+		cpt++;
+	}
+	return (cpt);
+}
