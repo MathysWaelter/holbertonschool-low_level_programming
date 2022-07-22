a0f20accdbe9a71d8670a997f553ce1ca062f8bf
Mathys Waelter <5005@holbertonschool.com>
2022-07-22 16:15:31 +0000

    add step 4

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * clear_bit - clear the value of a bit
+ * @n: value
+ * @index: placement
+ * Return: Value of the bit
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > (sizeof(n) * 8))
+		return (-1);
+
+	*n ~= 0 >> index;
+	return (1);
+}
