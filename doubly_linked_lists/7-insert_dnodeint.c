31b95a4bc2edb715d5b04ed98bf3180598b74f9b
Mathys Waelter <5005@holbertonschool.com>
2022-07-22 13:09:39 +0000

    add step 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -32,6 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 		node->n = n;
 		node->next = *h;
+		node->prev = ptr;
 		*h = node;
 		return (node);
 	}
