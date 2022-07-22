6300d60f9f4b893d91d9e5d0dd560ecdaaa51ea7
Mathys Waelter <5005@holbertonschool.com>
2022-07-22 16:38:57 +0000

    add step 101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{	
-	fprintf( stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+/**
+ * main - main prog
+ * Return: 1
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n");
+	return (1);
 }
