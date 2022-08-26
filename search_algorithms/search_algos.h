a6062fdd5a64dd0b17799df50de036087c979cd1
root <mathys.laurent3164@gmail.com>
2022-08-26 06:03:36 -0700

    add binary search fonction

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -4,5 +4,7 @@
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int binarySearch(int *array, int high, int low, int target);
 
 #endif
